cd8533fdc7a95d98eb6b5758c780a1ccf6c50a43
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2010-05-19 17:34:19 +1000

    Revised validator code.

--- lib/strong_uniqueness_validator.rb
+++ lib/strong_uniqueness_validator.rb
@@ -1,7 +1,15 @@
 class StrongUniquenessValidator < ActiveModel::EachValidator
-  def validate_each(record, attr, value)
-    if (record.class.where(attr.to_sym => /\A#{value}\Z/i) - [record]).any?
-       record.errors[attr] << 'already taken'
-     end
+  def validate_each(record, attribute, value)
+    if record.new_record? || key_changed?(record)
+      if record.class.where(attribute => /\A#{value}\Z/i).any?
+        record.errors[attribute] << 'already taken'
+      end
+    end
+  end
+
+  private
+
+  def key_changed?(record)
+    (record.primary_key || []).any? { |key| record.send("#{key}_changed?") }
   end
 end
