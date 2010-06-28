47a5663c3991d5bf5128ec2b868863347114a63b
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2010-06-28 17:25:21 +1000

    Added timestamps.rb

---
+++ lib/timestamps.rb
@@ -0,0 +1,26 @@
+module Timestamps
+  include SessionInfo
+
+  extend ActiveSupport::Concern
+  included do
+    field :created_at, :type => Time
+    field :updated_at, :type => Time
+    belongs_to_related :created_by, :class_name => 'User'
+    belongs_to_related :updated_by, :class_name => 'User'
+
+    set_callback :create, :before, :set_created_at
+    set_callback :update, :before, :set_updated_at
+  end
+
+  def set_created_at
+    if !created_at
+      self.created_at = Time.now.utc 
+      self.created_by = current_user
+    end
+  end
+  
+  def set_updated_at
+    self.updated_at = Time.now.utc
+    self.updated_by = current_user
+  end
+end
