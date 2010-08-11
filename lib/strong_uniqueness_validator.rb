18b2ec115e621ae68e47410d134ddcb09e5d6339
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2010-08-11 15:33:14 +1000

    StrongUniquenessValidator works with embedded records.

--- lib/strong_uniqueness_validator.rb
+++ lib/strong_uniqueness_validator.rb
@@ -1,9 +1,21 @@
 class StrongUniquenessValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     if record.new_record? || key_changed?(record)
-      if record.class.where(attribute => /\A#{value}\Z/i).any?
-        record.errors[attribute] << 'already taken'
-      end
+      if record.embedded?
+        assoc = record.associations.values.find do |v|
+          v.association == Mongoid::Associations::EmbeddedIn
+        end
+        if assoc and (parent = record.send(assoc.name))
+          children = parent.send(assoc.inverse_of) - [record]
+          if children.any? { |child| child.send(attribute) =~ /\A#{value}\Z/i }
+            record.errors[attribute] << 'already taken'
+          end
+        end
+      else
+        if record.class.where(attribute => /\A#{value}\Z/i).any?
+          record.errors[attribute] << 'already taken'
+        end
+      end    
     end
   end
 
