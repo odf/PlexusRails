a955f5c5f169feb6b2d9dd88eb64ac7060ee8550
odf <olaf.delgado@gmail.com>
2013-03-27 16:15:07 +1100

    Refined the sqlite3 recipe for capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,21 +22,10 @@ set :scm, :git
 set :repository, "git@github.com:odf/PlexusRails.git"
 set :branch,     "master"
 
+set :db_name, "production.sqlite3"
 set :rails_env, "production" # needed by migrations?
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
-
-after 'deploy:setup', :create_extra_dirs
-after 'deploy:update', :symlink_db
-
-desc "create additional shared directories during setup"
-task :create_extra_dirs, :roles => :app do
-  run "mkdir -m 0755 -p #{shared_path}/db"
-end
-
-task :symlink_db, :roles => :app do
-  run "ln -nfs #{shared_path}/db/* #{current_path}/db/"
-end
--- config/recipes/sqlite3.rb
+++ config/recipes/sqlite3.rb
@@ -5,4 +5,18 @@ namespace :sqlite3 do
     run "#{sudo} apt-get -y install sqlite3 libsqlite3-dev"
   end
   after "deploy:install", "sqlite3:install"
+
+  desc "Set up a blank sqlite3 database file"
+  task :setup, roles: :app do
+    run "mkdir -m 0755 -p #{shared_path}/db"
+    run "touch #{shared_path}/db/#{db_name}"
+    run "chmod 0600 #{shared_path}/db/#{db_name}"
+  end
+  after "deploy:setup", "sqlite3:setup"
+
+  desc "Link the database file into the current application path"
+  task :symlink, roles: :app do
+    run "ln -nfs #{shared_path}/db/#{db_name} #{current_path}/db/"
+  end
+  after "deploy:update", "sqlite3:symlink"
 end
